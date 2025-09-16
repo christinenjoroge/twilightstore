89b96b085e310a89462f18113b4a41dbe4bf80e9
christinenjorge <christinenjoroge79@gmail.com>
2025-09-16 22:17:10 +0300

    added a new folder assets added javascript function in the function.php

--- wp-content/themes/twilight/functions.php
+++ wp-content/themes/twilight/functions.php
@@ -8,7 +8,8 @@
 
 function twilight_enque_scripts (){
     wp_enqueue_style('stylesheet',get_stylesheet_uri(), [], filemtime(get_template_directory().'/style.css'),'all');
-}
+    wp_enqueue_script('main.js',get_template_directory_uri().'/assets/main.js', [],filemtime(get_template_directory().'/assets/main.js'),true );
+} 
 
 add_action('wp_enqueue_scripts','twilight_enque_scripts');
 
